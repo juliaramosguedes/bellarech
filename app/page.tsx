83b81211e31b02a9c70ff3450628835412c261b5
Julia Ramos <juliaramosguedes@gmail.com>
2023-01-05 13:46:51 -0300

    build: css frameworks

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1>
+    <h1 className="text-3xl font-bold underline">
       Welcome to Isabella's World
     </h1>
   )
